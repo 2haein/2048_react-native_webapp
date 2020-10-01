56c60650b83158f07c342e7e9ebce347b01f16e3
2haein <eehaein2@gmail.com>
2020-10-02 03:53:15 +0900

    build 수정

--- App.js
+++ App.js
@@ -12,8 +12,8 @@ export default class App extends React.Component {
   adFunction = () => {
     <AdMobBanner
     bannerSize="fullBanner"
-    adUnitID="ca-app-pub-3940256099942544/6300978111" // Test ID, Replace with your-admob-unit-id
-    // adUnitID="ca-app-pub-6128669615016913/8187315932"
+    // adUnitID="ca-app-pub-3940256099942544/6300978111" // Test ID, Replace with your-admob-unit-id
+    adUnitID="ca-app-pub-6128669615016913/8187315932"
     servePersonalizedAds // true or false
     onDidFailToReceiveAdWithError={this.bannerError} />
   }
